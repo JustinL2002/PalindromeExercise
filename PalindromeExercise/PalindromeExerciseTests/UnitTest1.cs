f0c8289066aa2cf5d34204bd549bb5c7f9a2a3cf
Justin Lawson <jmlawson2002@gmail.com>
2022-10-04 14:16:57 -0400

    palindrome excercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,14 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void IsAPalindrome_Test(string word, bool expected)
         {
-
+            var wordsmith = new WordSmith();
+            var actual = wordsmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ PalindromeExercise/WordSmith/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WordSmith
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
